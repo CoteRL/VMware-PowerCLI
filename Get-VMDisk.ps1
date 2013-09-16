0db5ca9f7b0bb6e102a7397444d13c2f691109f0
Bob <coterl@gmail.com>
2013-09-15 21:57:33 -0400

    SCSI Controller added to output

--- Get-VMDisk.ps1
+++ Get-VMDisk.ps1
@@ -1,4 +1,14 @@
 Function Get-VMDisk {
+<#
+.SYNOPSIS
+  Lists specific properties of VM disks
+.DESCRIPTION
+  The function will list VM Name, Disk Name, SCSI Controller ID, SCSI ID, and Persistence state for every disk of a VM or multiple VMs
+.NOTES
+  Authors: Bob Cote
+.PARAMETER VM
+  A virtual machine or multiple virtual machines
+#>
 	Param(  
 		[Parameter(Mandatory=$true,ValueFromPipeline=$true)]
 		$VM
@@ -11,8 +21,9 @@ Function Get-VMDisk {
 				$DiskProperties = New-Object PsObject
 				$DiskProperties | Add-Member Noteproperty VMName -Value $_.Name
 				$DiskProperties | Add-Member Noteproperty "Disk Name" -Value $Disk.Name
-				$ID = $VMView.Config.Hardware.Device | where {$_.GetType().Name -eq "VirtualDisk"} | where {$_.DeviceInfo.Label -eq $Disk.Name} | Select UnitNumber
-				$DiskProperties | Add-Member Noteproperty "SCSI ID" -Value $ID.UnitNumber
+				$Info = $VMView.Config.Hardware.Device | where {$_.GetType().Name -eq "VirtualDisk"} | where {$_.DeviceInfo.Label -eq $Disk.Name}
+				$DiskProperties | Add-Member Noteproperty "SCSI Controller" -Value $Info.ControllerKey
+				$DiskProperties | Add-Member Noteproperty "SCSI ID" -Value $Info.UnitNumber
 				$DiskProperties | Add-Member Noteproperty Persistence -Value $Disk.Persistence
 				$DiskProperties
 			}
